c2451fbba94e6dbd5136538d475f3ae741660d53
test <test@gmail.com>
2025-10-16 20:50:41 +0800

    Revert "usb"
    This reverts commit e2e5dba1f7d57ffe0363b8fdec913bc683237018.

--- main.cpp
+++ main.cpp
@@ -1,51 +1,104 @@
-#include <QtCore/QCoreApplication>
-#include <QtCore/QDebug>
-#include <libimobiledevice/libimobiledevice.h>
+#include "Logger.h"
+#include "MainWindow.h"
+#include "TcpServer.h"
+#include "UdpTransport.h"
+#include "NetworkUtils.h"
+#include "LogWindow.h"
+#include "DeviceInfo.h"
+#include "EventHub.h"
+#include <QApplication>
+#include <QLoggingCategory>
+#include <QShortcut>
+
+MainWindow* mainWindow;
+
+void onClientConnected(QTcpSocket* socket) {
+    // qDebugEx() << "有新的客户端连接！";
+}
+
+void onDataReceived(QTcpSocket* socket, const QJsonObject &jsonObject) {
+    auto event = jsonObject["event"].toString();
+    auto data = jsonObject["data"];
+
+    if (event == "ping")
+        return;
+
+    qDebugEx() << event << data;
+
+    EventHub::TriggerEvent(event, data, socket);
+}
+
+void onClientDisconnected(QTcpSocket* socket) {
+    // qDebugEx() << "客户端断开连接！";
+}
+
+void onError(QTcpSocket* socket, QAbstractSocket::SocketError socketError) {
+    qCriticalEx() << "发生错误：" << socketError;
+}
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication application(argc, argv);
 
-    idevice_t device = NULL;
-    idevice_connection_t connection = NULL;
+    // qputenv("QT_FFMPEG_DEBUG", "1");
 
-    // 连接 iPhone
-    if (IDEVICE_E_SUCCESS != idevice_new(&device, NULL)) {
-        qDebug() << "无法找到设备";
-        return -1;
+    QLoggingCategory::setFilterRules("qt.multimedia.*=true");
+
+    mainWindow = new MainWindow;
+    auto logWindow = new LogWindow(mainWindow);
+    logWindow->resize(mainWindow->size().width(), 400);
+    auto shortcut = new QShortcut(QKeySequence(Qt::Key_F5), mainWindow);
+    QObject::connect(shortcut, &QShortcut::activated, logWindow, &LogWindow::toggleVisibility);
+
+    TcpServer server(onClientConnected, onDataReceived, onClientDisconnected, onError);
+
+    QString localIP = NetworkUtils::getLocalIP();
+    qDebugEx() << "本机内网IP:" << localIP;
+
+    UdpTransport udpTransport(
+        [](const QJsonObject &jsonObject) {
+            qDebugEx() << "Received Data:" << jsonObject;
+        }
+    );
+
+    QList<QHostAddress> subnetIPs = NetworkUtils::getSubnetIPs(localIP);
+    for (const QHostAddress &ip : subnetIPs) {
+        // qDebugEx() << "同子网IP: " << ip.toString();
+        udpTransport.sendData(QJsonObject{{"ip", localIP}, {"port", server.serverPort()}}, ip, 32838);
     }
 
-    uint16_t port = 32839;
-    idevice_error_t err = idevice_connect(device, port, &connection);
+    QScreen *screen = QApplication::primaryScreen();
+    QRect screenGeometry = screen->availableGeometry();
 
-    if (err != IDEVICE_E_SUCCESS) {
-        qDebug() << "连接失败:" << err;
-        idevice_free(device);
-        return -1;
-    }
+    int x = (screenGeometry.width() - mainWindow->width()) / 2;
+    int y = (screenGeometry.height() - mainWindow->height()) / 2;
+    mainWindow->move(x, y);
 
-    qDebug() << "成功连接 iOS USB 端口" << port;
+    mainWindow->show();
+    // mainWindow->showMinimized();
 
-    // 发送数据
-    const char *test_data = "Hello from Qt over USB!";
-    uint32_t bytes_sent = 0;
-    err = idevice_connection_send(connection, test_data, strlen(test_data), &bytes_sent);
+    // QTimer::singleShot(3000, [&mainWindow](){
+    //     qDebugEx() << "singleShot";
+    //     mainWindow.addItem();
+    //     mainWindow.addItem();
+    //     mainWindow.addItem();
+    //     mainWindow.addItem();
+    //     mainWindow.addItem();
+    // });
 
-    if (err == IDEVICE_E_SUCCESS)
-        qDebug() << "成功发送数据，字节数:" << bytes_sent;
-    else
-        qDebug() << "发送失败:" << err;
+    // auto player = new DeviceWidget();
+    // // player->setSource(QString("tcp://192.168.0.102:23145"));
+    // mainWindow.addPlayer(player);
+    // mainWindow.addPlayer(new DeviceWidget());
+    // mainWindow.addPlayer(new DeviceWidget());
 
-    // 接收返回（可选）
-    char buffer[256] = {0};
-    uint32_t bytes_received = 0;
-    if (idevice_connection_receive_timeout(connection, buffer, sizeof(buffer)-1, &bytes_received, 2000) == IDEVICE_E_SUCCESS) {
-        qDebug() << "接收数据:" << buffer;
-    } else {
-        qDebug() << "2秒内未收到返回";
-    }
-
-    idevice_disconnect(connection);
-    idevice_free(device);
-    return 0;
+    return application.exec();
 }
+
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
+    return main(__argc, __argv);
+}
+#endif
