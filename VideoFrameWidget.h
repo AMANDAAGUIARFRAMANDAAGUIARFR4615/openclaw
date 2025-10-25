8ed74eb5d07bd8abbb18453c312160c86c3f0393
test <test@gmail.com>
2025-10-25 23:08:08 +0800

    多线程播放器

--- VideoFrameWidget.h
+++ VideoFrameWidget.h
@@ -12,6 +12,35 @@
 #include <QTimer>
 #include <QDragEnterEvent>
 #include <QDropEvent>
+#include <QThread>
+#include <QObject>
+
+// 新增：帧处理器类，运行在工作线程中
+class VideoFrameProcessor : public QObject
+{
+    Q_OBJECT
+public:
+    explicit VideoFrameProcessor(QObject *parent = nullptr) : QObject(parent) {}
+
+public slots:
+    void processFrame(const QVideoFrame &frame) {
+        if (!frame.isValid()) {
+            qCriticalEx() << "无效帧";
+            return;
+        }
+
+        QImage img = frame.toImage();
+        if (img.isNull()) {
+            qCriticalEx() << "frame.toImage()";
+            return;
+        }
+
+        emit frameProcessed(img); // 发送处理后的 QImage
+    }
+
+signals:
+    void frameProcessed(const QImage &image);
+};
 
 class VideoFrameWidget : public QWidget
 {
@@ -19,15 +48,35 @@ class VideoFrameWidget : public QWidget
 
 public:
     explicit VideoFrameWidget(QWidget *parent = nullptr) 
-        : QWidget(parent), mediaPlayer(new QMediaPlayer(this))
+        : QWidget(parent), 
+          mediaPlayer(nullptr),
+          videoSink(nullptr),
+          frameProcessor(nullptr), // 新增：帧处理器
+          workerThread(new QThread(this))
     {
-        videoSink = new QVideoSink(this);
+        // 初始化 QMediaPlayer、QVideoSink 和 VideoFrameProcessor
+        mediaPlayer = new QMediaPlayer();
+        videoSink = new QVideoSink();
+        frameProcessor = new VideoFrameProcessor(); // 新增：创建帧处理器
+        mediaPlayer->moveToThread(workerThread);
+        videoSink->moveToThread(workerThread);
+        frameProcessor->moveToThread(workerThread); // 新增：将帧处理器移到工作线程
+
+        // 启动线程
+        workerThread->start();
+
+        // 设置视频输出
         mediaPlayer->setVideoSink(videoSink);
 
-        connect(videoSink, &QVideoSink::videoFrameChanged, this, &VideoFrameWidget::onVideoFrameChanged);
+        // 连接视频帧信号到帧处理器
+        connect(videoSink, &QVideoSink::videoFrameChanged, frameProcessor, &VideoFrameProcessor::processFrame, Qt::QueuedConnection);
+
+        // 连接帧处理器到主线程的绘制
+        connect(frameProcessor, &VideoFrameProcessor::frameProcessed, this, &VideoFrameWidget::onFrameProcessed, Qt::QueuedConnection);
 
         mediaPlayer->setAudioOutput(nullptr);
 
+        // 连接媒体状态信号
         connect(mediaPlayer, &QMediaPlayer::mediaStatusChanged, this, [this](QMediaPlayer::MediaStatus status) {
             qDebugEx() << "Media Status Changed: " << status;
             if (status == QMediaPlayer::LoadedMedia) {
@@ -39,11 +88,24 @@ public:
                     mediaPlayer->play();
                 }
             }
-        });
+        }, Qt::QueuedConnection);
 
-        connect(mediaPlayer, &QMediaPlayer::errorChanged, [this]() {
-            qCriticalEx() << "errorChanged" << mediaPlayer->errorString();
-        });
+        // 连接错误信号
+        connect(mediaPlayer, &QMediaPlayer::errorOccurred, this, [this](QMediaPlayer::Error error, const QString &errorString) {
+            qCriticalEx() << "errorOccurred" << errorString;
+        }, Qt::QueuedConnection);
+
+        // 清理线程
+        connect(this, &QObject::destroyed, workerThread, &QThread::quit);
+        connect(workerThread, &QThread::finished, workerThread, &QObject::deleteLater);
+        connect(this, &QObject::destroyed, frameProcessor, &QObject::deleteLater); // 新增：清理帧处理器
+    }
+
+    ~VideoFrameWidget() override {
+        if (workerThread->isRunning()) {
+            workerThread->quit();
+            workerThread->wait();
+        }
     }
 
     void orientationChanged(int orientation)
@@ -81,7 +143,7 @@ public:
         }
     }
 
-    QMediaPlayer* const mediaPlayer;
+    QMediaPlayer *mediaPlayer;
 
 protected:
     void paintEvent(QPaintEvent *event) override
@@ -101,33 +163,22 @@ protected:
         auto rotatedImage = currentImage.transformed(transform);
         auto scaled = rotatedImage.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
 
-        // 计算居中位置
         QRect targetRect(QPoint(0, 0), scaled.size());
         targetRect.moveCenter(rect().center());
 
         painter.drawImage(targetRect.topLeft(), scaled);
     }
 
-    void onVideoFrameChanged(const QVideoFrame &frame)
+    // 新增：处理帧处理器发来的 QImage
+    void onFrameProcessed(const QImage &image)
     {
-        if (!frame.isValid())
-        {
-            qCriticalEx() << "无效帧";
-            return;
-        }
-
-        QImage img = frame.toImage();
-        if (img.isNull())
-        {
-            qCriticalEx() << "frame.toImage()";
-            return;
-        }
-
-        currentImage = img;
+        currentImage = image;
         update();
     }
 
     QVideoSink *videoSink;
     QImage currentImage;
     int rotationAngle = 0;
+    QThread *workerThread;
+    VideoFrameProcessor *frameProcessor; // 新增：帧处理器
 };
