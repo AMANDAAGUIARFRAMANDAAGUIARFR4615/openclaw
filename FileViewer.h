e459bd9aa2fe49370960021e7616834ce37ef474
test <test@gmail.com>
2025-10-29 18:51:52 +0800

    FileViewer

---
+++ FileViewer.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <QWidget>
+#include <QFileDialog>
+#include <QFile>
+#include <QTextEdit>
+#include <QLabel>
+#include <QMessageBox>
+#include <QImageReader>
+#include <QVBoxLayout>
+#include <QPixmap>
+#include <QFileInfo>
+
+class FileViewer : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit FileViewer(const QString &filePath, QWidget *parent = nullptr) : QWidget(parent)
+    {
+        setWindowTitle("文件预览器");
+        resize(parent ? parent->size() : size(800, 600));
+        openFile(filePath);
+    }
+
+private:
+    void openFile(const QString &fileName)
+    {
+        QVBoxLayout *layout = new QVBoxLayout(this);
+
+        if (isTextFile(fileName)) {
+            QFile file(fileName);
+            if (!file.open(QIODevice::Text)) {
+                QMessageBox::error(this, "错误", "无法打开文本文件。");
+                return;
+            }
+
+            QTextEdit *textEdit = new QTextEdit;
+            textEdit->setPlainText(QString::fromUtf8(file.readAll()));
+            layout->addWidget(textEdit);
+        }
+        else if (isImageFile(fileName)) {
+            QPixmap pixmap(fileName);
+            if (pixmap.isNull()) {
+                QMessageBox::error(this, "错误", "无法加载图片。");
+                return;
+            }
+
+            QLabel *imageLabel = new QLabel;
+            imageLabel->setAlignment(Qt::AlignCenter);
+            imageLabel->setPixmap(pixmap.scaled(size(), Qt::KeepAspectRatio, Qt::SmoothTransformation));
+            layout->addWidget(imageLabel);
+        }
+        else {
+            QMessageBox::information(this, "不支持的文件", "不支持该文件类型。");
+            return;
+        }
+
+        setLayout(layout);
+    }
+
+    bool isTextFile(const QString &fileName)
+    {
+        static const QStringList textExtensions = {"txt", "cpp", "h", "json", "md", "ini", "log", "csv", "recordx"};
+        return textExtensions.contains(QFileInfo(fileName).suffix().toLower());
+    }
+
+    bool isImageFile(const QString &fileName)
+    {
+        static const QStringList imageExtensions = {"jpg", "jpeg", "png", "bmp", "gif", "webp"};
+        return imageExtensions.contains(QFileInfo(fileName).suffix().toLower());
+    }
+};
