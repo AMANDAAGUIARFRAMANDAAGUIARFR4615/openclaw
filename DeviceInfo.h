09f86f2d9df5bfff54538a1183be9277a76bb2f4
test <test@gmail.com>
2025-11-08 10:34:20 +0800

    DeviceInfo.h

--- DeviceInfo.h
+++ DeviceInfo.h
@@ -8,8 +8,35 @@
 
 class DeviceInfo {
 public:
-    explicit DeviceInfo(const QJsonObject &json);
-    ~DeviceInfo();
+    explicit DeviceInfo(const QJsonObject &json)
+      : deviceId(json["deviceId"].toString()),
+        deviceName(json["deviceName"].toString()),
+        videoPort(json["videoPort"].toInt()),
+        jbType(json["jbType"].toInt()),
+        localIp(json["localIp"].toString()),
+        orientation(json["orientation"].toInt()),
+        model(json["model"].toString()),
+        systemVersion(json["systemVersion"].toString()),
+        screenWidth(json["screenWidth"].toInt()),
+        screenHeight(json["screenHeight"].toInt()),
+        lockedStatus(json["lockedStatus"].toBool()),
+        version(json["version"].toString()) {
+            
+        auto screenSize = QGuiApplication::primaryScreen()->size();
+        auto maxWidth = screenSize.width() * 0.8;
+        auto maxHeight = screenSize.height() * 0.8;
+
+        if (screenWidth > maxWidth || screenHeight > maxHeight) {
+            scaleFactor = std::min(maxWidth / static_cast<float>(screenWidth),
+                                maxHeight / static_cast<float>(screenHeight));
+        }
+
+        allDevices.append(this);
+    }
+    
+    ~DeviceInfo() {
+        allDevices.removeOne(this);
+    }
 
     const QString deviceId;
     const QString deviceName;
@@ -26,7 +53,18 @@ public:
     bool lockedStatus;
     float scaleFactor = 1;
 
-    QString uniqueName() const;
+    QString uniqueName() const {
+        int sameCount = 0;
+        for (auto dev : allDevices) {
+            if (dev->deviceName == deviceName)
+                sameCount++;
+        }
+
+        if (sameCount == 1)
+            return deviceName;
+
+        return QString("%1_%2").arg(deviceName, deviceId);
+    }
 
 private:
     static QList<DeviceInfo*> allDevices;
--- Statics.cpp
+++ Statics.cpp
@@ -1,5 +1,7 @@
+#include "DeviceInfo.h"
 #include "AppListWidget.h"
 #include "Recorder.h"
 
+QList<DeviceInfo*> DeviceInfo::allDevices;
 QMap<DeviceConnection*, AppListWidget*> AppListWidget::instanceMap;
 QMap<DeviceConnection*, Recorder*> Recorder::instanceMap;
