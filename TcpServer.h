bffc9dd602c520aac931ab3adafcd1313c8fa5a9
test <test@gmail.com>
2025-11-14 01:05:57 +0800

    TcpServer.h UdpTransport.h

--- TcpServer.h
+++ TcpServer.h
@@ -1,12 +1,14 @@
 #pragma once
 
-#include <QObject>
+#include "NetworkUtils.h"
 #include <QTcpServer>
 #include <QTcpSocket>
 #include <QByteArray>
 #include <QJsonObject>
 #include <QMap>
 #include <functional>
+#include <QJsonDocument>
+#include <QHostInfo>
 
 class TcpServer : public QTcpServer
 {
@@ -17,19 +19,81 @@ public:
               const std::function<void(QTcpSocket*, const QJsonObject&)> &onDataReceived,
               const std::function<void(QTcpSocket*)> &onClientDisconnected,
               const std::function<void(QTcpSocket*, QAbstractSocket::SocketError)> &onError,
-              quint16 port = 0);
+              quint16 port = 0)
+    : onDataReceivedCallback(onDataReceived),
+      onClientConnectedCallback(onClientConnected),
+      onClientDisconnectedCallback(onClientDisconnected),
+      onErrorCallback(onError)
+    {
+        instance = this;
+
+        connect(this, &QTcpServer::newConnection, this, &TcpServer::onNewConnection);
+
+        if (!this->listen(QHostAddress::AnyIPv4, port)) {
+            qCriticalEx() << "无法启动服务器" << port;
+            return;
+        }
+
+        qDebugEx() << "服务器已启动,监听端口:" << serverPort();
+    }
 
     static TcpServer* getInstance() {return instance;}
-    QJsonObject getHostInfo(const QString& ip = nullptr);
+
+    QJsonObject getHostInfo(const QString& ip = nullptr) {
+        return QJsonObject{{"ip", ip != nullptr ? ip : NetworkUtils::getLocalIP()}, {"port", serverPort()}, {"remoteDeviceName", QHostInfo::localHostName()}};
+    }
 
 private slots:
-    void onNewConnection();
-    void onReadyRead();
-    void onDisconnected();
-    void onErrorOccurred(QAbstractSocket::SocketError socketError);
+    void onNewConnection() {
+        auto socket = this->nextPendingConnection();
+
+        auto ip = socket->peerAddress().toString();
+        auto port = socket->peerPort();
+
+        qDebugEx() << "连接成功" << ip + ":" + QString::number(port);
+
+        connect(socket, &QTcpSocket::readyRead, this, &TcpServer::onReadyRead);
+        connect(socket, &QTcpSocket::disconnected, this, &TcpServer::onDisconnected);
+        connect(socket, &QTcpSocket::errorOccurred, this, &TcpServer::onErrorOccurred);
+
+        clientBuffers[socket] = QByteArray();
+        if (onClientConnectedCallback) {
+            onClientConnectedCallback(socket);
+        }
+    }
+
+    void onReadyRead() {
+        auto socket = qobject_cast<QTcpSocket*>(sender());
+        auto data = socket->readAll();
+        clientBuffers[socket].append(data);
+        processBufferedData(socket);
+    }
+
+    void onDisconnected() {
+        auto socket = qobject_cast<QTcpSocket*>(sender());
+    
+        auto ip = socket->peerAddress().toString();
+        auto port = socket->peerPort();
+
+        qDebugEx() << "连接断开" << ip + ":" + QString::number(port);
+
+        clientBuffers.remove(socket);
+        if (onClientDisconnectedCallback) {
+            onClientDisconnectedCallback(socket);
+        }
+        socket->deleteLater();
+    }
+
+    void onErrorOccurred(QAbstractSocket::SocketError socketError) {
+        auto socket = qobject_cast<QTcpSocket*>(sender());
+        qCriticalEx() << "Socket error:" << socketError;
+        if (onErrorCallback && socket) {
+            onErrorCallback(socket, socketError);
+        }
+    }
 
 private:
-    static TcpServer* instance;
+    inline static TcpServer* instance;
 
     QMap<QTcpSocket*, QByteArray> clientBuffers; // 保存每个客户端的缓冲区
     std::function<void(QTcpSocket*)> onClientConnectedCallback;
@@ -37,5 +101,36 @@ private:
     std::function<void(QTcpSocket*)> onClientDisconnectedCallback;
     std::function<void(QTcpSocket*, QAbstractSocket::SocketError)> onErrorCallback;
 
-    void processBufferedData(QTcpSocket* socket);
+    void processBufferedData(QTcpSocket* socket) {
+        auto &buffer = clientBuffers[socket];
+
+        while (buffer.size() >= sizeof(quint64) + sizeof(quint32)) {
+            auto identifier = *reinterpret_cast<quint64*>(buffer.data());
+            if (identifier != 0xb7c2e0f542a39a3e) {
+                qCriticalEx() << "识别码不匹配，丢弃数据" << QString("0x%1").arg(identifier, 0, 16);
+                buffer.clear(); // 清空缓冲区
+                return;
+            }
+
+            auto jsonDataLength = *reinterpret_cast<quint32*>(buffer.data() + sizeof(quint64));
+
+            if (buffer.size() < static_cast<int>(sizeof(quint64) + sizeof(quint32) + jsonDataLength)) {
+                // qDebugEx() << "数据不完整，等待更多数据" << buffer.size() << sizeof(quint64) + sizeof(quint32) + jsonDataLength;
+                return;
+            }
+
+            QByteArray jsonData = buffer.mid(sizeof(quint64) + sizeof(quint32), jsonDataLength);
+            // 移除已处理的数据包
+            buffer.remove(0, sizeof(quint64) + sizeof(quint32) + jsonDataLength);
+            QJsonDocument doc = QJsonDocument::fromJson(jsonData);
+            
+            if (!doc.isNull()) {
+                if (onDataReceivedCallback) {
+                    onDataReceivedCallback(socket, doc.object());
+                }
+            } else {
+                qCriticalEx() << "JSON 解析失败，丢弃数据";
+            }
+        }
+    }
 };
--- UdpTransport.h
+++ UdpTransport.h
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <QObject>
 #include <QUdpSocket>
 #include <functional>
 #include <QByteArray>
 #include <QJsonObject>
+#include <QTimer>
 
 class UdpTransport : public QObject
 {
@@ -13,17 +13,121 @@ class UdpTransport : public QObject
 public:
     UdpTransport(const std::function<void(const QJsonObject &jsonObject)> &onDataReceived,
               const std::function<void(QAbstractSocket::SocketError)> &onError = nullptr,
-              quint16 listenPort = 0);
+              quint16 listenPort = 0)
+    : onDataReceivedCallback(onDataReceived),
+      onErrorCallback(onError),
+      listenPort(listenPort)
+    {
+        socket = new QUdpSocket(this);
 
-    void sendData(const QJsonObject &jsonObject, const QHostAddress &host, quint16 port, quint16 retryCount = 5);
+        if (!socket->bind(QHostAddress::AnyIPv4, listenPort)) {
+            qCriticalEx() << "无法绑定端口";
+            return;
+        }
+
+        qDebugEx() << "udp绑定端口：" << socket->localPort();
+
+        connect(socket, &QUdpSocket::readyRead, this,  [this]() {
+            QByteArray receivedData;
+            while (socket->hasPendingDatagrams()) {
+                receivedData.resize(int(socket->pendingDatagramSize()));
+                socket->readDatagram(receivedData.data(), receivedData.size());
+
+                // 将接收到的数据缓存到缓冲区
+                buffer.append(receivedData);
+
+                // 尝试处理缓冲区中的数据包
+                processBufferedData();
+            }
+        });
+
+        connect(socket, &QUdpSocket::errorOccurred, this,  [this](QAbstractSocket::SocketError error) {
+            qCriticalEx() << "errorOccurred" << error << "|" << socket->errorString();
+
+            if (onErrorCallback) {
+                onErrorCallback(error);
+            }
+        });
+    }
+
+    void sendData(const QJsonObject &jsonObject, const QHostAddress &host, quint16 port, quint16 retryCount = 5) {
+        if (socket->state() != QAbstractSocket::BoundState) {
+            qCriticalEx() << "UDP 套接字未绑定，无法发送数据！";
+            return;
+        }
+
+        quint64 identifier = 0xc6e8f3de9a654d6b;
+
+        QByteArray jsonData = QJsonDocument(jsonObject).toJson();
+
+        quint32 jsonDataLength = jsonData.size();
+
+        QByteArray dataToSend;
+        dataToSend.append(reinterpret_cast<const char*>(&identifier), sizeof(identifier));
+        dataToSend.append(reinterpret_cast<const char*>(&jsonDataLength), sizeof(jsonDataLength));
+        dataToSend.append(jsonData);
+
+        auto sendAttempt = new std::function<void(quint16)>();
+        *sendAttempt = [=](quint16 remainingRetries) mutable {
+            if (socket->writeDatagram(dataToSend, host, port) == dataToSend.size()) {
+                delete sendAttempt; // 成功发送，清理
+                return;
+            }
+
+            if (remainingRetries == 0) {
+                qCriticalEx() << host.toString() + ":" + QString::number(port) << "发送失败，已达到最大重试次数！";
+                delete sendAttempt;
+                return;
+            }
+
+            qWarningEx() << host.toString() + ":" + QString::number(port) 
+                        << "发送失败，剩余重试次数:" << remainingRetries;
+
+            // 使用单次定时器延迟重试
+            QTimer::singleShot(50, [=]() {
+                (*sendAttempt)(remainingRetries - 1);
+            });
+        };
+
+        (*sendAttempt)(retryCount);
+    }
 
 private:
     std::function<void(const QJsonObject &jsonObject)> onDataReceivedCallback;
     std::function<void(QAbstractSocket::SocketError)> onErrorCallback;
+
+    void processBufferedData() {
+        while (buffer.size() >= sizeof(quint64) + sizeof(quint32)) {
+            quint64 identifier = *reinterpret_cast<quint64*>(buffer.data());
+            if (identifier != 0xb7c2e0f542a39a3e) {
+                qCriticalEx() << "识别码不匹配，丢弃数据" << QString("0x%1").arg(identifier, 0, 16);
+                buffer.clear(); // 清空缓冲区
+                return;
+            }
+
+            quint32 jsonDataLength = *reinterpret_cast<quint32*>(buffer.data() + sizeof(quint64));
+
+            if (buffer.size() < sizeof(quint64) + sizeof(quint32) + jsonDataLength) {
+                qDebugEx() << "数据不完整，等待更多数据" << buffer.size() << sizeof(quint64) + sizeof(quint32) + jsonDataLength;
+                return;
+            }
+
+            QByteArray jsonData = buffer.mid(sizeof(quint64) + sizeof(quint32), jsonDataLength);
+            // 移除已处理的数据包
+            buffer.remove(0, sizeof(quint64) + sizeof(quint32) + jsonDataLength);
+            QJsonDocument doc = QJsonDocument::fromJson(jsonData);
+            
+            if (!doc.isNull()) {
+                if (onDataReceivedCallback) {
+                    onDataReceivedCallback(doc.object());
+                }
+            } else {
+                qCriticalEx() << "JSON 解析失败，丢弃数据";
+            }
+        }
+    }
+
     quint16 listenPort;
-
     QUdpSocket *socket;
-
     QByteArray buffer;
-    void processBufferedData();
 };
