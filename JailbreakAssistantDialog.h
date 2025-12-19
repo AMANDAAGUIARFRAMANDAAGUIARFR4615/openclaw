ddc8b4e5dda845c97781440c3ad96b43a09aa3d1
test <test@gmail.com>
2025-12-19 10:51:50 +0800

    x

--- JailbreakAssistantDialog.h
+++ JailbreakAssistantDialog.h
@@ -527,7 +527,7 @@ public:
 
         auto step1 = new StepDownload("下载TrollRestore", "帮你给手机装上“巨魔商店”，解锁随意安装App且永不过期的强大功能。", url);
         auto step2 = new StepSelectApp("选择一个将被替换的系统应用", "请选择一个已安装在手机上的系统应用进行注入：");
-        auto step3 = new StepExecute("开始注入", "运行程序并将Helper注入到选中应用【请先确认已连接USB并信任电脑】");
+        auto step3 = new StepExecute("开始注入", "运行程序并将 Helper 注入目标应用。（请确保仅有一台手机连接并已信任此电脑）");
         auto step4 = new StepScan("使用相机app扫码下载Dopamine", "稳定且现代化的无根越狱工具", "https://gitee.com/coding202208/pandora/releases/download/v1/Dopamine.tipa");
         auto step5 = new StepFinal("最后一步", "在手机上打开巨魔商店（TrollStore）\n点击右上角加号\n选择Install IPA File就可以安装Dopamine\n安装后打开Dopamine点击越狱等待完成即可。");
 
