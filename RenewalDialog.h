59ae2524be29e4bcd8ffef33978cbdd9f5dab63d
test <test@gmail.com>
2025-12-24 20:08:43 +0800

    2

--- RenewalDialog.h
+++ RenewalDialog.h
@@ -28,11 +28,11 @@ public:
 
     explicit RenewalDialog(QWidget *parent) : QDialog(parent)
     {
-        setWindowTitle(tr("续费"));
+        setWindowTitle("续费");
         resize(650, 700);
 
         QHBoxLayout *filterLayout = new QHBoxLayout();
-        QLabel *lblFilter = new QLabel(tr("分组筛选:"));
+        QLabel *lblFilter = new QLabel("分组筛选:");
         m_comboFilter = new QComboBox();
 
         const auto& items = MainWindow::getInstance()->getTabs();
@@ -46,7 +46,7 @@ public:
 
         m_tableWidget = new QTableWidget(this);
         m_tableWidget->setColumnCount(3);
-        m_tableWidget->setHorizontalHeaderLabels({"", tr("设备名称"), tr("到期时间")});
+        m_tableWidget->setHorizontalHeaderLabels({"", "设备名称", "到期时间"});
         m_tableWidget->setFrameShape(QFrame::NoFrame);
         m_tableWidget->setShowGrid(false);
         m_tableWidget->setAlternatingRowColors(true);
@@ -70,12 +70,12 @@ public:
         QVBoxLayout *optionsLayout = new QVBoxLayout();
         optionsLayout->setSpacing(15);
 
-        QGroupBox *grpDuration = new QGroupBox(tr("续费周期"));
+        QGroupBox *grpDuration = new QGroupBox("续费周期");
         QHBoxLayout *layDuration = new QHBoxLayout(grpDuration);
 
-        m_radioMonth = new QRadioButton(tr("月付 (¥%1/台)").arg(BASE_PRICE_PER_MONTH));
+        m_radioMonth = new QRadioButton(QString("月付 (¥%1/台)").arg(BASE_PRICE_PER_MONTH));
         int yearPrice = BASE_PRICE_PER_MONTH * 12 / 2; // 年付5折
-        m_radioYear = new QRadioButton(tr("年付 (¥%1/台 - 5折特惠)").arg(yearPrice));
+        m_radioYear = new QRadioButton(QString("年付 (¥%1/台 - 5折特惠)").arg(yearPrice));
         m_radioYear->setStyleSheet("color: #d32f2f; font-weight: bold;");
         m_radioMonth->setChecked(true);
 
@@ -83,11 +83,11 @@ public:
         layDuration->addWidget(m_radioYear);
         layDuration->addStretch();
 
-        QGroupBox *grpVoucher = new QGroupBox(tr("代金券充值"));
+        QGroupBox *grpVoucher = new QGroupBox("代金券充值");
         QVBoxLayout *layVoucher = new QVBoxLayout(grpVoucher);
 
         QHBoxLayout *balanceLayout = new QHBoxLayout();
-        balanceLayout->addWidget(new QLabel(tr("当前可用余额:")));
+        balanceLayout->addWidget(new QLabel("当前可用余额:"));
         m_lblBalanceAmount = new QLabel();
         updateBalanceLabel();
         m_lblBalanceAmount->setStyleSheet("font-weight: bold; color: #E65100; font-size: 14px;");
@@ -96,11 +96,11 @@ public:
 
         QHBoxLayout *redeemLayout = new QHBoxLayout();
         m_editVoucherCode = new QPlainTextEdit();
-        m_editVoucherCode->setPlaceholderText(tr("请输入兑换码，每行一个..."));
+        m_editVoucherCode->setPlaceholderText("请输入兑换码，每行一个...");
         m_editVoucherCode->setFixedHeight(80);
         m_editVoucherCode->setStyleSheet("QPlainTextEdit { border: 1px solid #ccc; border-radius: 4px; padding: 4px; }");
 
-        m_btnRedeem = new QPushButton(tr("批量兑换"));
+        m_btnRedeem = new QPushButton("批量兑换");
         m_btnRedeem->setFixedHeight(80);
 
         redeemLayout->addWidget(m_editVoucherCode, 1);
@@ -109,19 +109,19 @@ public:
         layVoucher->addLayout(balanceLayout);
         layVoucher->addLayout(redeemLayout);
 
-        QGroupBox *grpPay = new QGroupBox(tr("付款方式"));
+        QGroupBox *grpPay = new QGroupBox("付款方式");
         QVBoxLayout *layPayMain = new QVBoxLayout(grpPay);
 
         QHBoxLayout *layPayRadios = new QHBoxLayout();
-        m_radioWeChat = new QRadioButton(tr("微信支付"));
-        m_radioVoucher = new QRadioButton(tr("余额支付"));
+        m_radioWeChat = new QRadioButton("微信支付");
+        m_radioVoucher = new QRadioButton("余额支付");
         m_radioWeChat->setChecked(true);
         layPayRadios->addWidget(m_radioWeChat);
         layPayRadios->addWidget(m_radioVoucher);
         layPayRadios->addStretch();
 
         QHBoxLayout *layTotal = new QHBoxLayout();
-        QLabel *lblTotalTitle = new QLabel(tr("应付总额:"));
+        QLabel *lblTotalTitle = new QLabel("应付总额:");
         lblTotalTitle->setStyleSheet("font-size: 14px; font-weight: bold;");
         m_lblTotalAmount = new QLabel("¥ 0.00");
         m_lblTotalAmount->setStyleSheet("font-size: 20px; font-weight: bold; color: #d32f2f;");
