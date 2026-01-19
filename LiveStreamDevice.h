3f4a700f96f02a52d146e5048192111df5f8010a
test <test@gmail.com>
2026-01-19 14:21:39 +0800

    QSharedPointer

--- LiveStreamDevice.h
+++ LiveStreamDevice.h
@@ -1,76 +1,76 @@
 #pragma once
 
-#include "Logger.h"
 #include <QIODevice>
 #include <QMutex>
 #include <QWaitCondition>
 #include <QByteArray>
-#include <QTcpSocket>
 #include <QDateTime>
+#include <QSharedPointer>
 
 class LiveStreamDevice : public QIODevice {
     Q_OBJECT
+
+    // 将所有线程共享的数据放入这个内部结构体
+    struct InternalData {
+        QMutex mutex;
+        QWaitCondition dataAvailable;
+        QByteArray buffer;
+        bool stopped = false;
+    };
+
 public:
-    explicit LiveStreamDevice(QObject *parent = nullptr) : QIODevice(parent) {}
+    explicit LiveStreamDevice(QObject *parent = nullptr) : QIODevice(parent) {
+        m_data = QSharedPointer<InternalData>::create();
+    }
 
     ~LiveStreamDevice() {
-        QMutexLocker locker(&m_mutex);
-        m_stopped = true;
-        m_dataAvailable.wakeAll();
+        QMutexLocker locker(&m_data->mutex);
+        m_data->stopped = true;
+        // 注意：即使这个析构函数执行完了，m_data 被销毁了，
+        // 只要 readData 里还有指针引用，InternalData 的内存就不会释放。
+        m_data->dataAvailable.wakeAll();
     }
 
     bool isSequential() const override { return true; }
 
     void appendData(const QByteArray &data) {
-        QMutexLocker locker(&m_mutex);
-        m_buffer.append(data);
-
-        quint64 sec = QDateTime::currentSecsSinceEpoch();
-        if (sec != m_lastSec) {
-            m_prevSecBytes = m_curSecBytes;
-            m_curSecBytes = 0;
-            m_lastSec = sec;
-        }
-        m_curSecBytes += data.size();
-
-        m_dataAvailable.wakeAll();
-    }
-
-    qint64 speedBps() const {
-        QMutexLocker locker(&m_mutex);
-        return m_prevSecBytes;
+        QMutexLocker locker(&m_data->mutex);
+        m_data->buffer.append(data);
+        m_data->dataAvailable.wakeAll();
     }
 
 protected:
     qint64 readData(char *data, qint64 maxSize) override {
-        QMutexLocker locker(&m_mutex);
+        // 【防崩溃的核心】：创建局部 shared_ptr
+        // 只要这个 d 变量还在作用域内，InternalData就绝对不会被销毁
+        auto d = m_data;
+        
+        QMutexLocker locker(&d->mutex);
 
-        while (m_buffer.isEmpty() && !m_stopped) {
-            m_dataAvailable.wait(&m_mutex);
+        while (d->buffer.isEmpty() && !d->stopped) {
+            // wait 会暂时解开锁。
+            // 此时如果主线程销毁了 LiveStreamDevice，m_data 成员变量没了，
+            // 但我们的局部变量 d 还在，所以 mutex 依然有效！
+            d->dataAvailable.wait(&d->mutex);
         }
 
-        if (m_stopped)
-            return -1;
+        if (d->stopped)
+            return -1; // 返回 -1 告诉播放器流结束了
 
-        qint64 bytesToRead = qMin(maxSize, qint64(m_buffer.size()));
-        memcpy(data, m_buffer.constData(), bytesToRead);
-        m_buffer.remove(0, bytesToRead);
+        qint64 bytesToRead = qMin(maxSize, qint64(d->buffer.size()));
+        memcpy(data, d->buffer.constData(), bytesToRead);
+        d->buffer.remove(0, bytesToRead);
+        
         return bytesToRead;
-    }
+    } // 函数结束，d 销毁。如果外部对象已析构，InternalData 在此处真正释放。
 
-    qint64 writeData(const char *, qint64) override {
-        return -1;
-    }
-
+    qint64 writeData(const char *, qint64) override { return -1; }
+    
     bool atEnd() const override {
         return false;
     }
 
-    QByteArray m_buffer;
-    mutable QMutex m_mutex;
-    QWaitCondition m_dataAvailable;
-    qint64 m_curSecBytes = 0;
-    qint64 m_prevSecBytes = 0;
-    quint64 m_lastSec = 0;
-    bool m_stopped = false;
+private:
+    // 唯一的成员变量，管理所有资源的生命周期
+    QSharedPointer<InternalData> m_data;
 };
