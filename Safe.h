04c51f2e32e5906e9c7489110fc7bf1cf5a17f4a
test <test@gmail.com>
2026-02-05 23:44:28 +0800

    HIDE_NUM

--- Safe.h
+++ Safe.h
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include <type_traits>
+#include <bit>
+#include <array>
 
 namespace DataGuard {
 
@@ -16,10 +18,14 @@ namespace DataGuard {
         mutable char m_buffer[N];
         uint32_t m_key;
 
+        // 使用 consteval 强制编译期执行 (C++20)
         template <uint32_t Len>
         consteval StrObfuscator(const char(&str)[Len]) : m_buffer{} {
             static_assert(Len <= N, "String literal is too long");
-            constexpr int seconds = []{ constexpr auto t = __TIME__; return ((t[0]-'0')*10+(t[1]-'0'))*3600 + ((t[3]-'0')*10+(t[4]-'0'))*60 + ((t[6]-'0')*10+(t[7]-'0')); }();
+            constexpr int seconds = []{ 
+                constexpr auto t = __TIME__; 
+                return ((t[0]-'0')*10+(t[1]-'0'))*3600 + ((t[3]-'0')*10+(t[4]-'0'))*60 + ((t[6]-'0')*10+(t[7]-'0')); 
+            }();
             m_key = compileTimeHash(str, seconds);
             encrypt(str, Len);
         }
@@ -52,15 +58,21 @@ namespace DataGuard {
     };
 
     // --- 数值混淆 ---
+    // 逻辑：将任何数值类型(int/float/double)视为字节数组，
+    // 使用与 StrObfuscator 完全相同的滚动异或算法进行混淆。
     template <typename T>
     struct NumObfuscator {
-        T m_val;
+        mutable uint8_t m_buffer[sizeof(T)];
         uint32_t m_key;
 
-        constexpr NumObfuscator(T val, uint32_t key) : m_val(val), m_key(key) {
-            // 整数用异或，浮点数用加法混淆
-            if constexpr (std::is_integral_v<T>) m_val ^= static_cast<T>(m_key);
-            else m_val += static_cast<T>(m_key);
+        consteval NumObfuscator(T val, uint32_t key) : m_buffer{}, m_key(key) {
+            // 将数值(无论是float还是int)转换为纯二进制字节数组
+            // std::bit_cast 是 C++20 中唯一能在 constexpr/consteval 上下文中进行类型这一转换的标准方法
+            auto bytes = std::bit_cast<std::array<uint8_t, sizeof(T)>>(val);
+
+            for (size_t i = 0; i < sizeof(T); ++i) {
+                m_buffer[i] = bytes[i] ^ static_cast<uint8_t>((m_key + i) * 71u);
+            }
         }
 
 #if defined(_MSC_VER)
@@ -69,11 +81,14 @@ namespace DataGuard {
         __attribute__((always_inline)) inline
 #endif
         T decrypt() const {
-            // 对应逆运算
-            if constexpr (std::is_integral_v<T>) return m_val ^ static_cast<T>(m_key);
-            else return m_val - static_cast<T>(m_key);
+            for (size_t i = 0; i < sizeof(T); ++i) {
+                m_buffer[i] ^= static_cast<uint8_t>((m_key + i) * 71u);
+            }
+            
+            return std::bit_cast<T>(m_buffer);
         }
 
+        // 隐式转换支持
         operator T() const { return decrypt(); }
     };
 }
@@ -82,4 +97,4 @@ namespace DataGuard {
 #define HIDE_STR(str) (DataGuard::StrObfuscator<sizeof(str)>(str, DataGuard::compileTimeHash(__TIME__, __LINE__)).decrypt())
 
 // 数值保护宏
-#define HIDE_NUM(val) (DataGuard::NumObfuscator<decltype(val)>(val, DataGuard::compileTimeHash(__TIME__, __LINE__)).decrypt())
+#define HIDE_NUM(val) (DataGuard::NumObfuscator<std::remove_cv_t<decltype(val)>>(val, DataGuard::compileTimeHash(__TIME__, __LINE__)).decrypt())
