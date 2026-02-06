d847fe6adc5750aed4173493591041a958d3e015
test <test@gmail.com>
2026-02-06 14:34:28 +0800

    fix

--- main.cpp
+++ main.cpp
@@ -148,13 +148,13 @@ int main(int argc, char *argv[])
         webSocketClient->emitEvent("reconnect", Account::getInstance()->id, [=](const QJsonValue &res) {
             for (const QJsonValue& device: res[HIDE_STR("devices")].toArray()) {
                 const auto& udid = device[HIDE_STR("udid")].toString();
-                const auto& expireAt = jsonValue[HIDE_STR("expireAt")].toInteger();
+                const auto& expireAt = device[HIDE_STR("expireAt")].toInteger();
                 DeviceInfo::expirations[udid] = expireAt;
                 DeviceInfo::setLocker(udid, device["locker"].toString());
 
                 auto deviceInfo = DeviceInfo::getDevice(udid);
                 if (deviceInfo)
-                    deviceInfo->expireAt = jsonValue[HIDE_STR("expireAt")].toInteger();
+                    deviceInfo->expireAt = expireAt;
             }
         });
     });
