53bf40e764d1100d49d8351e60d5296fb54b833d
test <test@gmail.com>
2026-05-11 12:24:10 +0800

    2.8.3

--- global.h
+++ global.h
@@ -33,5 +33,5 @@ namespace Config {
     const int SERVER_PORT = 9000;
 
     const QString SITE_URL = "https://remotepro.cn/";
-    const QString VERSION = "2.8.2";
+    const QString VERSION = "2.8.3";
 }
